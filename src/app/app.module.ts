3e2da22d411a10f524c8fb720e24efac9fd742b3
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-04-18 18:18:26 +0300

    add components imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'
+import { MatInputModule } from '@angular/material/input'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserReposComponent } from './user-repos/user-repos.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { RepoAgePipe } from './repo-age.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserReposComponent,
+    UserInputComponent,
+    RepoAgePipe
   ],
   imports: [
+    MatInputModule,
+    MatButtonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
