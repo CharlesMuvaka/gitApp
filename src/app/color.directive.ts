4a22289f15670e71b967767b04b968e414a49fbe
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-04-19 16:13:07 +0300

    st:change the font-size of the pipe

--- src/app/color.directive.ts
+++ src/app/color.directive.ts
@@ -8,7 +8,7 @@ export class ColorDirective {
   constructor(private color:ElementRef) { 
 
     this.color.nativeElement.style.color = 'greenyellow';
-    this.color.nativeElement.style.fontSize = '20px';
+    this.color.nativeElement.style.fontSize = '15px';
     this.color.nativeElement.style.fontWeight = 'bold';
     this.color.nativeElement.style.fontStyle = 'italic';
   }
--- src/app/repo-age.pipe.ts
+++ src/app/repo-age.pipe.ts
@@ -6,20 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RepoAgePipe implements PipeTransform {
 
   transform(value: any): number {
-    let today: Date = new Date();
-    let todayActualDate:any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-    var dateDiff = Math.abs(value(value.getFullYear(), value.getMonth(), value.getDate()) - todayActualDate);
-    const daySecs = 86400;
-    let datediffToSecs = dateDiff*0.001;
-    let repoAged = datediffToSecs/daySecs;
-
-    if(repoAged > 0 && value > repoAged){
-      return repoAged;
-      
-    }else{
-      return 0;
-
-    }
+    let valueArray = value.replace(/(\d{4})-(\d{2})-(\d{2}).*/ig,"$1 $2 $3").split(" ");
+	    let dateCreated = new Date(valueArray[0],valueArray[1]-1, valueArray[2]);
+	    let now = new Date() ;
+	    let difference = now.getTime() - dateCreated.getTime();
+	    difference/=(1000 * 60 * 60 * 24);
+	    return Math.floor(difference);
   }
 
 }
