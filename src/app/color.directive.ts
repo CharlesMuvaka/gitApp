d453df73e9ce10e72d6e2f88812162bd981c91f8
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-04-19 13:35:05 +0300

    create a color directive

---
+++ src/app/color.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appColor]'
+})
+export class ColorDirective {
+
+  constructor(private color:ElementRef) { 
+
+    this.color.nativeElement.style.color = 'greenyellow';
+    this.color.nativeElement.style.fontSize = '20px';
+    this.color.nativeElement.style.fontWeight = 'bold';
+    this.color.nativeElement.style.fontStyle = 'italic';
+  }
+
+}
