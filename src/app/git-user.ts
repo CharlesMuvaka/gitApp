667f15295f24da3d33d7fd96468b1a143a980b44
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-04-19 13:35:46 +0300

    create a blueprint for github users

---
+++ src/app/git-user.ts
@@ -0,0 +1,5 @@
+export class GitUser {
+
+
+    constructor(public gitUserName:string, public gitUserProfileImg:string, public userRepos:string, public dateCreated:string, public dateUpdate:string, public repoUrl:string){}
+}
